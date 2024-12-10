35a1a3fc99cdd0171eb4445679b5d6326e140c56
Suresh Tamang <sureshjimba3333@gmail.com>
2024-12-10 21:41:27 +0545

    Uploading the sourcecode

---
+++ orgeno/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('home/',views.home,name='home'),
+    path('base/',views.base, name='base'),
+]
---
+++ orgeno/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(requests):
+    return render(requests, 'index.html')
+
+
+def base(requests):
+    return render(requests, 'base.html')
